875a9a2bc9a00e60ec8d0dc1f68606025ae747b1
Inna <innababiichuk88@gmail.com>
2017-08-03 16:27:21 +0000

    Some styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
     def destroy
         @post = Post.find(params[:post_id])
         @comment = @post.comments.find(params[:id])
-        @comment.detroy
+        @comment.destroy
         redirect_to post_path(@post)
     end
 end
